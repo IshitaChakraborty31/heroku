923096b2735e4e1d0e3071d87aba2d1d73e714c2
IshitaChakraborty31 <ishita31031996@gmail.com>
2022-01-12 18:09:31 +0530

    updates

--- public/js/script.js
+++ public/js/script.js
@@ -4,9 +4,9 @@ let icon= document.getElementById("icon");
 icon.onclick= function(){
     if(mySong.paused){
         mySong.play();
-        icon.src="12.png";
+        icon.src="img/12.png";
     }else{
         mySong.pause();
-        icon.src="22.png";
+        icon.src="img/22.png";
     }
 }
